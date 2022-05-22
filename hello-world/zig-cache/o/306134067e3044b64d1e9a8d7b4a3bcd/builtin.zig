0b42503f7e6c1c42f06c7966b016c770da8ecee8
kapralovs <ser-kapralov@yandex.ru>
2022-05-22 23:20:17 +0300

    Initial commit

---
+++ hello-world/zig-cache/o/306134067e3044b64d1e9a8d7b4a3bcd/builtin.zig
@@ -0,0 +1,84 @@
+const std = @import("std");
+/// Zig version. When writing code that supports multiple versions of Zig, prefer
+/// feature detection (i.e. with `@hasDecl` or `@hasField`) over version checks.
+pub const zig_version = std.SemanticVersion.parse("0.9.1") catch unreachable;
+/// Temporary until self-hosted is feature complete.
+pub const zig_is_stage2 = false;
+/// Temporary until self-hosted supports the `cpu.arch` value.
+pub const stage2_arch: std.Target.Cpu.Arch = .aarch64;
+/// Temporary until self-hosted can call `std.Target.x86.featureSetHas` at comptime.
+pub const stage2_x86_cx16 = false;
+
+pub const output_mode = std.builtin.OutputMode.Exe;
+pub const link_mode = std.builtin.LinkMode.Static;
+pub const is_test = false;
+pub const single_threaded = false;
+pub const abi = std.Target.Abi.musl;
+pub const cpu: std.Target.Cpu = .{
+    .arch = .aarch64,
+    .model = &std.Target.aarch64.cpu.cortex_a76,
+    .features = std.Target.aarch64.featureSet(&[_]std.Target.aarch64.Feature{
+        .a76,
+        .aes,
+        .ccpp,
+        .contextidr_el2,
+        .crc,
+        .crypto,
+        .dotprod,
+        .fp_armv8,
+        .fullfp16,
+        .fuse_aes,
+        .lor,
+        .lse,
+        .neon,
+        .pan,
+        .pan_rwv,
+        .ras,
+        .rcpc,
+        .rdm,
+        .sha2,
+        .ssbs,
+        .uaops,
+        .v8_1a,
+        .v8_2a,
+        .v8a,
+        .vh,
+    }),
+};
+pub const os = std.Target.Os{
+    .tag = .linux,
+    .version_range = .{ .linux = .{
+        .range = .{
+            .min = .{
+                .major = 4,
+                .minor = 19,
+                .patch = 113,
+            },
+            .max = .{
+                .major = 4,
+                .minor = 19,
+                .patch = 113,
+            },
+        },
+        .glibc = .{
+            .major = 2,
+            .minor = 19,
+            .patch = 0,
+        },
+    }},
+};
+pub const target = std.Target{
+    .cpu = cpu,
+    .os = os,
+    .abi = abi,
+};
+pub const object_format = std.Target.ObjectFormat.elf;
+pub const mode = std.builtin.Mode.Debug;
+pub const link_libc = false;
+pub const link_libcpp = false;
+pub const have_error_return_tracing = true;
+pub const valgrind_support = false;
+pub const position_independent_code = false;
+pub const position_independent_executable = false;
+pub const strip_debug_info = false;
+pub const code_model = std.builtin.CodeModel.default;
